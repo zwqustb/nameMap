660b1be7573567619ff4d5d0fc78960856e868ba
zwqustb <522976741@qq.com>
2017-06-15 09:44:24 +0800

    init commit

---
+++ nameMap/nameMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  nameMap
+//
+//  Created by zhangwenqiang on 2017/6/15.
+//  Copyright © 2017年 zhangwenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
